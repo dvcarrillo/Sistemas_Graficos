5668de35bf40418b2683501117fa2b75d20e568b
Andres Molina Lopez <andresmag2@gmail.com>
2018-04-10 16:39:54 +0200

    Class ovo working

--- Segunda práctica/project/src/Ovo.js
+++ Segunda práctica/project/src/Ovo.js
@@ -18,17 +18,20 @@ class Ovo extends THREE.Object3D {
         
         this.object = this.createObject();
         this.add(this.object);
+        console.log(this.object.position.x);
     }
 
     createObject() {
         const precision = 30;   // Number of radial segments
 
         // Creates the sphere
-        var objectGeometry = new THREE.SphereGeometry(this.headRadius, precision, precision);
-        var object = new THREE.Mesh(headGeometry, 0xFF0000);
+        var objectGeometry = new THREE.SphereGeometry(this.ovoRadius, precision, precision);
+        var object = new THREE.Mesh(objectGeometry, new THREE.MeshPhongMaterial({ color: 0xff0000, specular: 0xfbf804, shininess: 0 }));
 
         // Positions the head over the body
         object.castShadow = true;
+        object.position.x = 10;
+        object.position.y = 5;
 
         return object;
     }
